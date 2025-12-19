ffdee0bf6021d8ff999863ad6c094d27a415518d
akash06959 <187724245+akash06959@users.noreply.github.com>
2025-12-19 11:41:49 +0530

    Update db.ts

--- web-app/src/lib/db.ts
+++ web-app/src/lib/db.ts
@@ -2,6 +2,10 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    // Add this block to support the ?sslmode=require in your URL
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
